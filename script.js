302e6d14f1f638db2a9c8f86470268d620a51935
Faria Mollika <149615629+MollikaFaria06@users.noreply.github.com>
2025-08-29 15:39:12 +0600

    added copy, clearand call buttons funtionality with coin count and copy count

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Heart Buttons Functionality
 const counterElement = document.querySelector(".count-hearts");
 let heartCount = 0;
 
@@ -18,3 +19,132 @@ for(let i = 0; i < heartButtons.length; i++){
         counterElement.innerText = heartCount;
     });
 }
+
+// Coin and Call Histories Functionality.
+
+const callButtons = document.querySelectorAll(".btn");
+const coinElement = document.getElementById("coin-count");
+let coin = parseInt(coinElement.innerText);
+const historyListContainer = document.getElementById("history-list");
+
+
+for(let i = 0; i < callButtons.length; i++){
+    const btn = callButtons[i];
+    
+    if(btn.innerText.includes("Call")){
+        btn.addEventListener("click",function(){
+        if(coin < 20){
+            alert("Sorry!!You don't have enough coins to make this call.");
+            return;
+        }
+
+        coin = coin - 20;
+        coinElement.innerText = coin;
+       
+        let serviceCard = btn.parentNode;
+        while (serviceCard && !(serviceCard.classList.contains("flex") && serviceCard.classList.contains("flex-col"))){
+            serviceCard = serviceCard.parentNode;
+        }
+
+        let headings = serviceCard.getElementsByTagName("h1");
+        let serviceName = "";
+        let serviceNumber = "";
+
+        for(let j = 0; j < headings.length; j++){
+            let h = headings[j];
+            if (h.classList.contains("font-bold") && h.classList.contains("text-xl")) {
+            serviceName = h.innerText;
+             break;
+             }
+
+        }
+
+        for (let k = 0; k < headings.length; k++) {
+            let h = headings[k];
+            if (h.classList.contains("font-bold") && h.classList.contains("text-2xl")) {
+            serviceNumber = h.innerText; 
+            break;
+            }
+        }
+
+        alert("Calling " + serviceName + " at number " + serviceNumber);
+
+        const date = new Date();
+        const timeString = date.toLocaleTimeString();
+ 
+        let history = document.createElement("div");
+        history.style.display = "flex";
+        history.style.justifyContent = "space-between";
+        history.style.alignItems ="center";
+        history.style.marginTop = "8px";
+        history.style.padding = "8px";
+        history.style.backgroundColor = "#f3f3f3";
+        history.style.borderRadius = "8px";
+
+        let info = document.createElement("div");
+        let name = document.createElement("div");
+        name.style.fontWeight = "bold";
+        name.innerText = serviceName;
+
+        let number = document.createElement("div");
+        number.style.fontSize = "14px";
+        number.style.color = "#555";
+        number.innerText = serviceNumber;
+
+        info.appendChild(name);
+        info.appendChild(number);
+
+        let time = document.createElement("div");
+        time.style.fontSize = "14px";
+        time.style.color = "black";
+        time.innerText = timeString;
+
+        history.appendChild(info);
+        history.appendChild(time);
+
+        historyListContainer.appendChild(history);
+    
+    });
+   
+    // Clean Buttons Functionality.
+
+    const clearHistoryBtn = document.getElementById("clear-history");
+
+    clearHistoryBtn.addEventListener("click",function(){
+        historyListContainer.innerHTML = "";
+    });
+ }  
+}
+
+// Copy count element and counter Functionality.
+let copyCount = 0;
+let copyCountElement = document.getElementById("copy-count");
+
+// Loop all buttons
+let copyButtons = document.getElementsByClassName("btn");
+for (let i = 0; i < copyButtons.length; i++) {
+    (function(index) {
+        let btn = copyButtons[index];
+
+        if (btn.innerText.indexOf("Copy") !== -1) {
+            btn.addEventListener("click", function() {
+                let serviceCard = btn.closest(".flex.flex-col");
+                let serviceNumberElement = serviceCard.querySelector("h1.text-2xl");
+                let serviceNumber = serviceNumberElement.innerText;
+
+                let tempInput = document.createElement("textarea");
+                tempInput.value = serviceNumber;
+                document.body.appendChild(tempInput);
+                tempInput.select();
+                document.execCommand("copy");
+                document.body.removeChild(tempInput);
+
+                alert("Number is copied: " + serviceNumber);
+                copyCount++;
+                copyCountElement.innerText = copyCount + " Copy";
+            });
+        }
+
+    })(i);
+}
+
